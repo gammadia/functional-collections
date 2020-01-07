ce6057da30655a1b73b9b2200f0aed9c061a1ba1
Dorian Villet <dorian.villet@gammadia.ch>
2020-01-07 15:08:19 +0100

    Upgrade Rector, add symfony50-types and type-declaration sets, import root namespace classes, simplify iterable PHPDocs.

--- vendor-unmanaged/gammadia/collections/src/Functional/Util.php
+++ vendor-unmanaged/gammadia/collections/src/Functional/Util.php
@@ -4,19 +4,38 @@ declare(strict_types=1);
 
 namespace Gammadia\Collections\Functional;
 
+use Traversable;
+use UnexpectedValueException;
+
 final class Util
 {
     /**
+     * An iterable can be either a \Traversable or an array (which cannot be passed to iterator_to_array() for example)
+     *
+     * @param mixed $iterable
+     *
+     * @return iterable<mixed>
+     */
+    public static function assertIterable($iterable): iterable
+    {
+        if (is_iterable($iterable)) {
+            return $iterable;
+        }
+
+        throw new UnexpectedValueException('Iterable expected');
+    }
+
+    /**
      * @param mixed $traversable
      *
-     * @return mixed
+     * @return \Traversable<mixed>
      */
-    public static function assertIterable($traversable)
+    public static function assertTraversable($traversable): Traversable
     {
-        if (is_iterable($traversable)) {
+        if ($traversable instanceof Traversable) {
             return $traversable;
         }
 
-        throw new \UnexpectedValueException('Traversable expected');
+        throw new UnexpectedValueException('Traversable expected');
     }
 }
